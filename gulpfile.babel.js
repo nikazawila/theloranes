b1d8011236a8b32d34537d8f68e2210f633338c2
Nika Zawila <w.zawila@gmail.com>
2015-12-06 20:00:32 +0100

    add gh-pages-gulp script and task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 // generated on 2015-12-04 using generator-gulp-webapp 1.0.3
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
+import ghPages from 'gulp-gh-pages';
 import browserSync from 'browser-sync';
 import del from 'del';
 import {stream as wiredep} from 'wiredep';
@@ -153,6 +154,11 @@ gulp.task('wiredep', () => {
       ignorePath: /^(\.\.\/)*\.\./
     }))
     .pipe(gulp.dest('app'));
+});
+
+gulp.task('deploy', () => {
+  gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
 
 gulp.task('build', ['html', 'images', 'fonts', 'extras'], () => {
